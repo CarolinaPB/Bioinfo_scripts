b26d8a1a7a5b2607a391a910ec8dcd9a5e198653
CarolinaPB <carolina.pitabarros@gmail.com>
2021-06-30 09:03:22 +0200

    function for ggplot boxplot stats

--- misc_Rfunctions.R
+++ misc_Rfunctions.R
@@ -30,3 +30,33 @@ gffRead <- function(gffFile, nrows = -1) {
       paste(sapply(gff, class), collapse=", "), "\n")
   return(gff)
 }
+
+# get list with values from boxplot like whisker, quantiles, etc
+ggplot2_boxplot_stats <- function(x){
+
+    quartiles <- as.numeric(quantile(x,
+                                     probs = c(0.25, 0.5, 0.75)))
+
+    names(quartiles) <- c("25th percentile",
+                          "50th percentile\n(median)",
+                          "75th percentile")
+
+    IQR <- diff(quartiles[c(1,3)])
+
+    upper_whisker <- max(x[x < (quartiles[3] + 1.5 * IQR)])
+    lower_whisker <- min(x[x > (quartiles[1] - 1.5 * IQR)])
+
+    upper_dots <- x[x > (quartiles[3] + 1.5*IQR)]
+    lower_dots <- x[x < (quartiles[1] - 1.5*IQR)]
+
+    return(list("quartiles" = quartiles,
+                "25th percentile" = as.numeric(quartiles[1]),
+                "50th percentile\n(median)" = as.numeric(quartiles[2]),
+                "75th percentile" = as.numeric(quartiles[3]),
+                "IQR" = IQR,
+                "upper_whisker" = upper_whisker,
+                "lower_whisker" = lower_whisker,
+                "upper_dots" = upper_dots,
+                "lower_dots" = lower_dots))
+  }
+
